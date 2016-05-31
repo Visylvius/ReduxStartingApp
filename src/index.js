f311387ccd88a9750a134b069f47d2cc818bcd3e
Loren Barrick <lorenbarrick@gmail.com>
2016-05-30 17:20:35 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Create a new component This component should produce
+//some HTML
+const app = function() {
+  return <div>Hi!</div>
+}
+//Take this components generated HTML and put it
+//on the page (DOM)
+ReactDOM.render(App);
