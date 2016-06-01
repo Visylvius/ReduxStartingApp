df5e0f935fb8859e91ec1ce769e1b1ace602adb2
Loren Barrick <lorenbarrick@gmail.com>
2016-05-31 17:57:34 -0700

    pass props to children, render images, and description to li

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 const API_KEY = 'your api key here';
 
-// Create a new component This component should produce
-//some HTML
-const App = () => {
-  return (
-    <div>
-     <SearchBar />
-    </div>
-  );
-};
 
-//Take this components generated HTML and put it
-//on the page (DOM)
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      //is equal to this.setState({ videos: videos })
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+       <SearchBar />
+       <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
