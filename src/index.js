4e4d19c2268081c5aec85a796e6e3013a6445df7
Loren Barrick <lorenbarrick@gmail.com>
2016-06-01 12:41:39 -0700

    throttling search, search bar now wired to accept input, display videos, and able to select videos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 const API_KEY = 'your api key here';
 
 
@@ -11,19 +13,31 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      //is equal to this.setState({ videos: videos })
-      this.setState({ videos });
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
+    const videoSearch = _.debounce( (term) => { this.videoSearch(term)}, 300);
     return (
       <div>
-       <SearchBar />
-       <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
